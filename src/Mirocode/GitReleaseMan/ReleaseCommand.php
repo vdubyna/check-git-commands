b8586bcb34bfb58cbd5c48738b5ace89d588cf28
Volodymyr Dubyna <vdubyna@speroteck.com>
2017-06-29 16:41:07 +0300

    Improve functionality

--- src/Mirocode/GitReleaseMan/ReleaseCommand.php
+++ src/Mirocode/GitReleaseMan/ReleaseCommand.php
@@ -14,6 +14,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
+use Composer\Semver\Semver;
 
 class ReleaseCommand extends Command
 {
@@ -69,20 +70,26 @@ class ReleaseCommand extends Command
         try {
             $this->_executeShellCommand('git fetch origin', $output);
             $this->_executeShellCommand('git reset --hard origin/master', $output);
-            $this->_executeShellCommand('git clean -fdx', $output);
+            $this->_executeShellCommand('git clean -fd', $output);
         } catch (ProcessFailedException $e) {
             $output->write($e->getMessage());
             $output->write('Stop the release process and exit.' . PHP_EOL);
             return;
         }
 
+        // Get current release version
+        // Make new tag or branch
+        // Push to repository
+        // ask to continue and prepare for release
+        $question = new ConfirmationQuestion('Do you want to continue? (y/n): ', false);
+        if (!$this->getHelper('question')->ask($input, $output, $question)) {
+            $output->write('Stop the release process and exit.' . PHP_EOL);
+            return;
+        }
 
-
-
-        // Generate release branch
-        //
-
-        // Get list of branches to merge and release.
+        $versions = explode(PHP_EOL, $this->_executeShellCommand('git tag -l', $output));
+        print_r($versions);
+        Semver::sort($versions);
 
         //$client = new \Github\Client();
         //$client->authenticate('16991e61d491933ead32fd870ac11df9f5d797ee', null, \Github\Client::AUTH_URL_TOKEN);
@@ -95,12 +102,18 @@ class ReleaseCommand extends Command
     }
 
     /**
+     *
+     * @param                 $cmd
      * @param OutputInterface $output
+     *
+     * @return string
      */
     protected function _executeShellCommand($cmd, OutputInterface $output)
     {
         $process = new Process($cmd);
         $process->mustRun();
         $output->write($process->getOutput());
+
+        return $process->getOutput();
     }
 }
